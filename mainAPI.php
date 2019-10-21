867e218010b19579a5d133081fcafda1ac991baa
Alexandra Jones <alexandra.e@hotmail.co.uk>
2019-10-21 09:52:26 +0100

    started

---
+++ mainAPI.php
@@ -0,0 +1,15 @@
+<?php
+//https://stackoverflow.com/questions/10751227/remove-image-background-with-php-and-save-transparent-png
+//https://stackoverflow.com/questions/7263923/how-to-force-file-download-with-php
+//https://www.php.net/manual/en/function.header.php
+
+echo "<h1>Working?</h1>";
+
+if(isset($_GET['res']))
+{
+    echo $_GET['res'];
+}
+
+
+
+?>
