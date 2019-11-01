3ffa3da7407756841cfa711c00be1c02272e2de3
Thomas Misson <11tmisson@gmail.com>
2019-11-01 19:28:42 +0000

    almost, just got to pipe file in

--- server.js
+++ server.js
@@ -1,83 +1,73 @@
-//https://medium.com/the-node-js-collection/simple-server-side-cache-for-express-js-with-node-js-45ff296ca0f0
+//http://localhost:3000/?img=01_04_2019_001103.pngresX=1000&resY=1000&ext=jpg
 
 let express = require('express')
 let app = express()
 let jimp = require('jimp');
 let NodeCache = require( "node-cache" );
 let myCache = new NodeCache( { stdTTL: 7200, checkperiod: 600 } );
+let fs = require('fs');
 
-app.get('/', cache(10), function (request, resonse) {
+
+app.get('/', function (request, response) {
     //Collects the paramter values from the request
-    let res = request.query.res; 
+    let resX = request.query.resX; 
+    let resY = request.query.resY; 
     let bgcol = request.query.bgcol; 
     let watermark = request.query.watermark; 
     let ext = request.query.ext;
+    let img = request.query.img;
 
-    let img = "01_04_2019_001103.png";
 
     try
     {
         //Tries to parse to number
-        res = Number(res);
+        resX = Number(resX);
+        resY = Number(resY);
 
         //If it can't value will be NaN
-        if((!isNaN(res))||watermark.length <=20||/^[0-9A-F]{6}$/i.test(bgcol)||(ext==="jpg"||ext==="png"||ext==="gif"||ext==="tiff"))
+        if((img !=undefined &&ext!=undefined&&resX!=undefined&&resY!=undefined)&&(!isNaN(resX)&&!isNaN(resY))||watermark.length <=20||/^[0-9A-F]{6}$/i.test(bgcol)||(ext==="jpg"||ext==="png"||ext==="tiff"||ext==="gif"||ext==="bmp"))
         {
-            console.log("Load: "+ img+ "   " + res + " " + watermark+ " "+ bgcol + " " + ext)
-
-            let resX = res;
-            let resY = (res/16)*9
-
-            console.log("Aspect ratio: 16:9   resX="+resX+" resY="+resY);
-        }
-        else
-        {
-            resonse.status(204).send();//No content if validation of data fails
-        }
-    }
-    catch(error){
-        resonse.status(400).send();//Any issues parsing data then 400
-    }
-    finally{
-        key=img+ext+res+watermark+bgcol;
-
-        //if key doesn't exist in the cache, create the image again 
-        if(myCache.get( key ) == undefined)
-        {
-            if(res!=undefined)
+            console.log("Load: "+ img+ "   " + resX +" "+ resY+ " " + watermark+ " "+ bgcol + " " + ext)
+        
+            key=img+ext+resX+resY+watermark+bgcol;
+ 
+            //if key doesn't exist in the cache, create the image again 
+            if(myCache.get( key ) == undefined)
             {
 
-            }
-            else if(watermark!=undefined)
-            {
-                
-            }
-            else if(ext!=undefined)
-            {
+                imageObj = jimp.read("product_images/"+img, (err, image) => {
+                    if (err) throw err;
 
-            }
-            else if(bgcol!=undefined)
-            {
-
-            }
-
-            //Tries to cache 
-            if(myCache.set( key, img, 7200  ))
-            {
-                resonse.status(201).send();
+                    image.resize(resX, resY)// resize
+                    .write('output-images/output.'+ext);// save to disk
+                });
+            
+                //Tries to cache 
+                if(myCache.set( key, imageObj, 7200  ))
+                {
+                    
+                    fs.createReadStream(wordsFile).pipe(response);//Created new content in cache
+                    response.status(201);
+                }
+                else
+                {
+                    response.status(500).send();//If unable to cache result  
+                }
             }
             else
             {
-                resonse.status(500).send();//If unable to cache result  
+                imageFromCache = myCache.get(key);
+                response.status(200).sendfile();
             }
         }
         else
         {
-            imageFromCache = myCache.get(key);
-            response.pipe();
-            resonse.status(200).send();
+            response.status(204).send({error:"Invalid data entered"});//No content if validation of data fails
         }
     }
-})
+    catch(error){
+        response.status(500);//Any issues parsing data then 400
+    }
+}) 
 
 app.listen(3000)
