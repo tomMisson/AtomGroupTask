47cc15346bafa99df832416475b90a353c8b38eb
Alexandra Jones <alexandra.e@hotmail.co.uk>
2019-11-04 16:58:52 +0000

    using pipes but unhandled promise

---
+++ README.md
@@ -0,0 +1,27 @@
+# Atom group - Image processing task
+
+## Installation
+
+To install all dependencies, install node.js, navigate to the same directory as package.json then run
+
+```bash
+npm install
+```
+___
+
+## Running
+
+To run the API, run the following:
+
+```bash
+node server 
+```
+
+Then to test it is running enter the following in to a browser:
+
+```
+http://localhost:3000/?img=01_04_2019_001103.png&resX=400&resY=400&ext=jpg
+```
+___
+
+Created by [Tom Misson](https://thomas.misson.info)
--- server.js
+++ server.js
@@ -1,73 +1,65 @@
-//http://localhost:3000/?img=01_04_2019_001103.pngresX=1000&resY=1000&ext=jpg
-
 let express = require('express')
 let app = express()
 let jimp = require('jimp');
-let NodeCache = require( "node-cache" );
-let myCache = new NodeCache( { stdTTL: 7200, checkperiod: 600 } );
-let fs = require('fs');
+let NodeCache = require("node-cache");
+let myCache = new NodeCache({ stdTTL: 7200, checkperiod: 600 });
+let data;
 
 
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
     //Collects the paramter values from the request
-    let resX = request.query.resX; 
-    let resY = request.query.resY; 
-    let bgcol = request.query.bgcol; 
-    let watermark = request.query.watermark; 
+    //params for url - resX - x resolution of new image,resY - y resolution of new image,bgcol,watermark - watermar for accross the image,ext - extension,img - image to use
+    let resX = request.query.resX;
+    let resY = request.query.resY;
+    let bgcol = request.query.bgcol;
+    let watermark = request.query.watermark;
     let ext = request.query.ext;
     let img = request.query.img;
 
 
-    try
-    {
+
+    try {
         //Tries to parse to number
         resX = Number(resX);
         resY = Number(resY);
 
         //If it can't value will be NaN
-        if((img !=undefined &&ext!=undefined&&resX!=undefined&&resY!=undefined)&&(!isNaN(resX)&&!isNaN(resY))||watermark.length <=20||/^[0-9A-F]{6}$/i.test(bgcol)||(ext==="jpg"||ext==="png"||ext==="tiff"||ext==="gif"||ext==="bmp"))
-        {
-            console.log("Load: "+ img+ "   " + resX +" "+ resY+ " " + watermark+ " "+ bgcol + " " + ext)
-        
-            key=img+ext+resX+resY+watermark+bgcol;
- 
+        if ((img != undefined && ext != undefined && resX != undefined && resY != undefined) && (!isNaN(resX) && !isNaN(resY)) || watermark.length <= 20 || /^[0-9A-F]{6}$/i.test(bgcol) || (ext === "jpeg" || ext === "png" || ext === "bmp")) {
+            console.log("Load: " + img + "   " + resX + " " + resY + " " + watermark + " " + bgcol + " " + ext)
+
+            key = ext.concat(+img + resX + resY + watermark + bgcol);
+
+            //Try to retrive from cache
+            cacheBuffer = myCache.get(key);
+
             //if key doesn't exist in the cache, create the image again 
-            if(myCache.get( key ) == undefined)
-            {
+            if (cacheBuffer == undefined) {
+                //If it can't create an instance of the 
+                imageObj = jimp.read("product_images/" + img, (err, image) => {
+                    if (err) {
+                        response.status(404).send({ 'error': "File not found" });
+                        throw err;
+                    };
 
-                imageObj = jimp.read("product_images/"+img, (err, image) => {
-                    if (err) throw err;
+                    image.resize(resX, resY) // resize
+                        .getBufferAsync('image/' + ext)
+                        .then(buff => stream.pipe(buff))
+                        .then(buff => myCache.set(key, buff, 7200))
+                        .then(response.status(201))
+                        .catch(err => res.status(500).send(err));
 
-                    image.resize(resX, resY)// resize
-                    .write('output-images/output.'+ext);// save to disk
                 });
-            
-                //Tries to cache 
-                if(myCache.set( key, imageObj, 7200  ))
-                {
-                    
-                    fs.createReadStream(wordsFile).pipe(response);//Created new content in cache
-                    response.status(201);
-                }
-                else
-                {
-                    response.status(500).send();//If unable to cache result  
-                }
+            } else {
+                //Pull from cache
+                stream.pipe(cacheBuffer);
             }
-            else
-            {
-                imageFromCache = myCache.get(key);
-                response.status(200).sendfile();
-            }
+        } else {
+            response.status(204).send({ 'error': "Missing or invalid data" }); //No content if validation of data fails
         }
-        else
-        {
-            response.status(204).send({error:"Invalid data entered"});//No content if validation of data fails
-        }
+    } catch (error) {
+        console.log(error)
+        response.status(500).send({ 'error': "Error server side" }); //Any issues parsing data then 400
     }
-    catch(error){
-        response.status(500);//Any issues parsing data then 400
-    }
-}) 
+})
 
 app.listen(3000)
